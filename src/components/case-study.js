f91f452621ed9aedcf2fe8f124a0643bb1add963
twillzy <william.tanzy@gmail.com>
2018-01-28 14:33:59 +1100

    yarn add rambda and toUpper

--- src/components/case-study.js
+++ src/components/case-study.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { toUpper } from 'ramda';
 
 export default ({ caseStudy }) => {
   const { title, description } = caseStudy;
   return <a href="" className="flex-box">
     <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/placeholder_square.png" alt="" />
-    <h1 className="flex-title">{title}</h1>
+    <h1 className="flex-title">{toUpper(title)}</h1>
     <p>{description}</p>
   </a>
 };
