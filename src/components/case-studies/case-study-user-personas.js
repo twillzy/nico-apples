bf33d78ee62c09e69e7bdacd230f5bd8ca990ed5
twillzy <william.tanzy@gmail.com>
2018-02-06 18:22:12 +1100

    user personas second one

--- src/components/case-studies/case-study-user-personas.js
+++ src/components/case-studies/case-study-user-personas.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import persona1 from '../../assets/images/drinkr-persona-1.png';
+import persona2 from '../../assets/images/drinkr-persona-2.png';
 
 export default ({ caseStudy }) => {
   const {
@@ -10,7 +11,8 @@ export default ({ caseStudy }) => {
       <div className="margin-bottom-sm narrow-container ">
         <h1 className="">User Personas</h1>
         <p className="margin-bottom-sm line-height-md">We developed two personas to synthesise our research. Both personas were created based on our initial interviews. One persona would represent the customer perspective while the other one would represent the bar perspective.</p>
-        <img className="width-100 border-bottom-red" src={persona1} alt="" />
+        <img className="margin-bottom-sm width-100 border-bottom-red" src={persona1} alt="" />
+        <img className="width-100 border-bottom-red" src={persona2} alt="" />
       </div>
     </div>
   );
