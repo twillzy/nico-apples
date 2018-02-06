283bc556f8d4976f3cf6b052be786635efca590f
twillzy <william.tanzy@gmail.com>
2018-02-07 02:59:31 +1100

    update user journey for drinkr

--- src/components/case-studies/case-study-user-journey.js
+++ src/components/case-studies/case-study-user-journey.js
@@ -8,8 +8,8 @@ export default ({ caseStudy }) => {
     <div className="case-study-overview white-bg padding-top-sm padding-bottom-sm">
       <div className="margin-bottom-sm narrow-container ">
         <h1 className="">User Journey</h1>
-        <p className="margin-bottom-sm line-height-md">I then mapped out a typical user journey for Charlotte breaking down the process of when she goes out to a bar. The user journey map shows shows the experience before and after our proposed solution.</p>
-        <img className="margin-bottom-sm width-100 border-bottom-red" src="https://static.wixstatic.com/media/bbeaac_87b3fc3efea54948809d297dc5e16d6a~mv2.png/v1/fill/w_1200,h_300,al_c,usm_0.66_1.00_0.01/bbeaac_87b3fc3efea54948809d297dc5e16d6a~mv2.png" alt="" />
+        <p className="margin-bottom-sm line-height-md">I then mapped out a typical user journey for Charlotte breaking down the process of when she goes out to a bar. The user journey map shows the experience before our proposed solution, highlighting all the pain points.</p>
+        <img className="margin-bottom-sm width-100 border-bottom-red" src="https://s3-ap-southeast-2.amazonaws.com/nicoapples-images/charlotte+journey.png" alt="" />
       </div>
     </div>
   );
