f4d9bff6e350ca6e2f87ad62809b359074a4d380
twillzy <william.tanzy@gmail.com>
2018-02-06 18:34:02 +1100

    scroll to top

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import {
   BrowserRouter as Router,
   Route,
 } from 'react-router-dom';
+import ScrollToTop from './components/scroll-to-top';
 import HomePage from './components/home-page';
 import DrinkrPage from './components/case-study-detailed-page';
 
 const App = () => (
   <Router>
-    <div>
+    <ScrollToTop>
       <Route exact path="/" component={HomePage} />
       <Route exact path="/case-studies/:id" component={DrinkrPage} />
-    </div>
+    </ScrollToTop>
   </Router >
 );
 
