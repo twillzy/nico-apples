5874887197e6a7cf098d269b12bffc472480396e
twillzy <william.tanzy@gmail.com>
2018-02-10 22:55:40 +1100

    Revert "about me page redo"
    This reverts commit f49c181b74a41b716aca5cf347d4d2eebe401395.

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -1,26 +1,19 @@
 import React from 'react';
 
 export default () => (
-  <div className="white-bg padding-bottom-sm">
-    <div className="margin-bottom-sm">
-      <div className="flex porcelain-bg">
-        <div className="lg-flex none lg-span-column-6 justify-content-center align-items-center">
-          <h1 className="padding-horizontal-xsm center font-lg">I AM A USER EXPERIENCE DESIGNER</h1>
-        </div>
+  <div className="white-bg padding-top-sm padding-bottom-sm">
+    <div className="wide-container margin-bottom-sm">
+      <h1>About Me</h1>
+      <div className="row">
         <div className="lg-span-column-6">
-          <img className="width-100" src="https://s3-ap-southeast-2.amazonaws.com/nicoapples-images/nico.JPG" alt="" />
+          <p className="margin-top-zero line-height-md">My name is Nico, I am a User Experience Designer and creative problem solver based in Sydney. After working as a marketing professional for 7 years, I recently transitioned to the world of UX to satisfy my thirst for creativity and combine it with existing skillsets. I spent many years trying to understand the users’ needs, now I design solutions for them. I am passionate about creating digital experiences that get to the core of the problem and put the users’ needs at the centre of the design process.</p>
+          <p className="line-height-md">I am also the proud owner of Cool As A Fox, an e-commerce selling colourful, gender-neutral socks.I started the business with my partner as an outlet to express my creativity and in just two years it has grown from a small side project to a successful e-commerce with over 4000 happy customers in Australia.</p>
+          <p className="line-height-md">I also happen to love pizza (without pineapple) and cats. I'd love to tell you more about what gets me excited, let's grab a coffee.</p>
+          <p className="line-height-md"><strong>WHY NICO APPLES?</strong> <span role="img" aria-label="apple">🍎 </span></p>
+          <p className="line-height-md">I love eating apples and I love apple products, but that’s not the reason behind my nickname. It’s a lot simpler than that: my last name means apples in Italian. I randomly picked it as my instagram handle a few years ago, and my friends started to call me nicoapples so I got stuck with it.</p>
         </div>
+        <img className="width-100 lg-span-column-6" src="https://s3-ap-southeast-2.amazonaws.com/nicoapples-images/nico.JPG" alt="" />
       </div>
-    </div>
-
-    <div className="padding-vertical-sm narrow-container">
-      <p className="margin-top-zero line-height-lg">My name is Nico, I am a User Experience Designer and creative problem solver based in Sydney. After working as a marketing professional for 7 years, I recently transitioned to the world of UX to satisfy my thirst for creativity and combine it with existing skillsets. I spent many years trying to understand the users’ needs, now I design solutions for them. I am passionate about creating digital experiences that get to the core of the problem and put the users’ needs at the centre of the design process.</p>
-      <p className="line-height-lg">I am also the proud owner of <a className="nico-red" href="https://coolasafox.com/">Cool As A Fox</a>, an e-commerce selling colourful, gender-neutral socks.I started the business with my partner as an outlet to express my creativity and in just two years it has grown from a small side project to a successful e-commerce with over 4000 happy customers in Australia.</p>
-      <p className="line-height-lg">I also happen to love pizza (without pineapple) and cats. I'd love to tell you more about what gets me excited, let's grab a coffee.</p>
-      <p className="line-height-lg"><strong>WHY NICO APPLES?</strong> <span role="img" aria-label="apple">🍎 </span></p>
-      <p className="line-height-lg">I love eating apples and I love apple products, but that’s not the reason behind my nickname. It’s a lot simpler than that: my last name means apples in Italian. I randomly picked it as my instagram handle a few years ago, and my friends started to call me nicoapples so I got stuck with it.</p>
     </div>
   </div>
 );
-
-
