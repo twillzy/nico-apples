779df37feb42cb9cf54a6e29a648e1f6abaf4659
twillzy <william.tanzy@gmail.com>
2018-02-11 15:55:52 +1100

    pixel pushing

--- src/components/case-studies/case-study-research.js
+++ src/components/case-studies/case-study-research.js
@@ -5,9 +5,9 @@ export default ({ caseStudy }) => {
     <div className="case-study-research white-bg padding-top-sm padding-bottom-sm">
       <div className="narrow-container margin-bottom-sm">
         <h1>Understanding the Users</h1>
-        <h4 className="section-header">Interviews and Surveys</h4>
       </div>
       <div className="narrow-container">
+        <h4 className="section-header">Interviews and Surveys</h4>
         <p className="margin-bottom-sm line-height-md">We started our preliminary research by interviewing people who go to bars regularly to understand their current experience. We conducted additional interviews with bar employees to get an additional perspective. We soon realised that some frustrations were related to having to wait for a long time to order drinks when a venue is busy. We then put out a survey to validate the patterns we identified during the interviews.</p>
         <img className="margin-bottom-sm width-100" src="https://s3-ap-southeast-2.amazonaws.com/nicoapples-images/drinkr/interview-stats.png" alt="" />
         <img className="width-100 border-bottom-red" src="https://s3-ap-southeast-2.amazonaws.com/nicoapples-images/drinkr/interview.jpg" alt="" />
