2e56aa42b79a33674d9287ca5ec716ae48a13443
Will Tan <will@spaceship.com.au>
2018-03-25 21:46:29 +1100

    reordering

--- src/components/case-study.js
+++ src/components/case-study.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default ({ caseStudy }) => {
-  const { id, title, description, imageUrl, projectType } = caseStudy;
+  const { id, year, title, description, imageUrl, projectType } = caseStudy;
   return (
     <div className="case-study">
       <Link to={`/case-studies/${id}`} className="grid-item">
@@ -10,11 +10,14 @@ export default ({ caseStudy }) => {
       </Link>
       <Link to={`/case-studies/${id}`} className="grid-item grid-item-big">
         <div className="case-study-content">
-          <h1 className="case-study-title">
-            {title}
-          </h1>
-          <p>2017 | <span>{projectType}</span><br />{description}</p>
+          <h1 className="case-study-title">{title}</h1>
+          <p>
+            {year} | <span>{projectType}</span>
+            <br />
+            {description}
+          </p>
         </div>
       </Link>
-    </div>);
+    </div>
+  );
 };
