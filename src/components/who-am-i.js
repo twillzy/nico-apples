26c7eb1a6ad364cdccb44263a90fdf3698904584
Will Tan <will@spaceship.com.au>
2018-03-25 18:22:26 +1100

    styling fun times again

--- src/components/who-am-i.js
+++ src/components/who-am-i.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React from "react";
 
-export default ({ name, location, jobTitle, message, toggleHoverState, isHelloHovered }) => <div className={`hero ${isHelloHovered ? " titus" : ""}`}>
-  <div className="hero-content">
-    <span onMouseEnter={toggleHoverState} onMouseLeave={toggleHoverState} className="hello">Hello!</span>
-    <div className="who-am-i">
-      <span>{`I am ${name}- a fabulous ${jobTitle} and creative problem solver based in ${location}. In the UX world full of Michelles, I strive to be like Beyoncé.`}</span>
+export default ({
+  name,
+  location,
+  jobTitle,
+  message,
+  toggleHoverState,
+  isHelloHovered
+}) => (
+  <div className={`hero ${isHelloHovered ? " titus" : ""}`}>
+    <div className="hero-content screen-wrapper">
+      <span
+        onMouseEnter={toggleHoverState}
+        onMouseLeave={toggleHoverState}
+        className="hello"
+      >
+        Hello!
+      </span>
+      <div className="who-am-i">
+        <span
+        >{`I am ${name}- a fabulous ${jobTitle} and creative problem solver based in ${location}. In the UX world full of Michelles, I strive to be like Beyoncé.`}</span>
+      </div>
     </div>
-  </div >
-</div >;
+  </div>
+);
