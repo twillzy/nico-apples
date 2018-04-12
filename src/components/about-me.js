db13a2fefa15e73536048ca0e283678985a345af
Will Tan <william.tanzy@gmail.com>
2018-04-12 20:03:30 +1000

    same height image and text on large screen

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -3,15 +3,17 @@ import React from 'react';
 export default () => (
   <div className="white-bg padding-top-sm">
     <div className="wide-container margin-bottom-sm">
-      <div className="row">
-        <div className="lg-span-column-6">
-          <h1 className="margin-top-zero">About Me</h1>
+      <h1 className="margin-top-zero">About Me</h1>
+      <div className="about-me">
+        <div className="about-me-text">
           <p className="margin-top-zero line-height-md">I’m a User Experience Designer and creative problem solver based in Sydney. After working as a marketing professional for 7 years, I recently transitioned to the world of UX design to satisfy my thirst for creativity and combine it with existing skillsets. I spent many years trying to understand the users’ needs, now I design solutions for them. I’m passionate about creating digital experiences that get to the core of the problem and put the users’ needs at the centre of the design process.</p>
           <p className="line-height-md">I am also the proud owner of <span className="nico-red"><a className="nostyle-link nostyle-visited" href="https://coolasafox.com/" target="_blank" rel="noopener noreferrer">Cool As A Fox</a></span>, an e-commerce selling colourful, gender-neutral socks. I started the business as an outlet to express my creative side and in just two years it has grown from a small side project to a successful e-commerce with over 4000 happy customers in Australia.</p>
           <p className="line-height-md">I also happen to love pizza (without pineapple) and cats. Check out my case studies and my <span className="nico-red"><a className="nostyle-link nostyle-visited" href="https://www.linkedin.com/in/nicomele/" target="_blank" rel="noopener noreferrer">LinkedIn</a></span> if you want to know more about my work. I’d love to tell you more about what gets me excited, <span className="nico-red"><a className="nostyle-link nostyle-visited" href="mailto:nicomele1@gmail.com">get in touch</a></span> for any question.</p>
           <p className="line-height-md">Let’s grab a coffee.</p>
         </div>
-        <img className="width-100 lg-span-column-6" src="https://s3-ap-southeast-2.amazonaws.com/nicoapples-images/nico-v2.jpg" alt="" />
+        <div className="about-me-img-container">
+          <img src="https://s3-ap-southeast-2.amazonaws.com/nicoapples-images/nico-v2.jpg" alt="" />
+        </div>
       </div>
     </div>
 
