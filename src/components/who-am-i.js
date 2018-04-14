40c765a689e1ab496c6b5c896cfdd6bdda4f5c7e
Will Tan <william.tanzy@gmail.com>
2018-04-14 17:04:01 +1000

    beyonce!

--- src/components/who-am-i.js
+++ src/components/who-am-i.js
@@ -9,17 +9,10 @@ export default ({
   isHelloHovered
 }) => (
   <div className={`hero ${isHelloHovered ? " titus" : ""}`}>
-    <div className="hero-content screen-wrapper">
-      <span
-        onMouseEnter={toggleHoverState}
-        onMouseLeave={toggleHoverState}
-        className="hello"
-      >
-        Hello!
-      </span>
+    <div className="hero-content screen-wrapper" onMouseEnter={toggleHoverState} onMouseLeave={toggleHoverState}>
+      <span className="hello">Hello!</span>
       <div className="who-am-i">
-        <span
-        >{`I am ${name}- a fabulous ${jobTitle} and creative problem solver based in ${location}. In the UX world full of Michelles, I strive to be like Beyoncé.`}</span>
+        <span>{`I am ${name}- a fabulous ${jobTitle} and creative problem solver based in ${location}. In the UX world full of Michelles, I strive to be like Beyoncé.`}</span>
       </div>
     </div>
   </div>
