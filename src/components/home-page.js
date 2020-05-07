668a119b7ead5e09cc8b8f53884381325cd86734
Will Tan <william.tanzy@gmail.com>
2020-05-07 11:24:03 +1000

    build: revive?

--- src/components/home-page.js
+++ src/components/home-page.js
@@ -1,40 +1,39 @@
-import React, { Component } from 'react';
-import WhoAmI from './who-am-i';
-import CaseStudies from './case-studies';
-import { caseStudies } from '../data/case-studies.json';
+import React, { Component } from "react";
+import WhoAmI from "./who-am-i";
+import CaseStudies from "./case-studies";
+import { caseStudies } from "../data/case-studies.json";
 
 class HomePage extends Component {
   constructor() {
     super();
     this.state = {
       isHelloHovered: false,
-    }
+    };
   }
 
-  toggleHoverState = () => this.setState({ isHelloHovered: !this.state.isHelloHovered });
+  toggleHoverState = () =>
+    this.setState({ isHelloHovered: !this.state.isHelloHovered });
 
   onShowMenuButtonClicked = (event) => {
     event.preventDefault();
     this.setState({ showMenuButton: !this.state.showMenuButton });
-  }
+  };
 
   render() {
     return (
       <div className="App">
         <WhoAmI
-          name="Nico"
+          name="Nicoo"
           location="Sydney"
           jobTitle="UX Designer"
           message="Don't ask me to make a button bigger. Ask me why the button should be bigger."
           toggleHoverState={this.toggleHoverState}
           isHelloHovered={this.state.isHelloHovered}
         />
-        <CaseStudies
-          caseStudies={caseStudies}
-        />
+        <CaseStudies caseStudies={caseStudies} />
       </div>
     );
   }
 }
 
-export default HomePage
+export default HomePage;
